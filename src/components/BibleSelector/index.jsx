45ca4674f756ef2505c099f203c248c03364221c
Sam Turner <turnertechappdeveloper@gmail.com>
2020-09-23 08:30:38 -0400

    Scaffold components for app

---
+++ src/components/BibleSelector/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function BibleSelector() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
---
+++ src/components/BookSelector/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function BookSelector() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
