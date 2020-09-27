450fb2e83a67c602d4fb8f43af4920dcd252cc40
Sam Turner <turnertechappdeveloper@gmail.com>
2020-09-27 00:13:36 -0400

    Updates

---
+++ precache-manifest.5538e9655b14bb8048a02ede8b665e16.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29407c34b874eb177f597caf03df21d1",
+    "url": "/bible-reader-react/index.html"
+  },
+  {
+    "revision": "6ce63b649279212f00e3",
+    "url": "/bible-reader-react/static/css/main.ccf817b1.chunk.css"
+  },
+  {
+    "revision": "75422a787c3b9dae36d0",
+    "url": "/bible-reader-react/static/js/2.3e389ba8.chunk.js"
+  },
+  {
+    "revision": "f2161a1a94beb91ff20e0491a834a114",
+    "url": "/bible-reader-react/static/js/2.3e389ba8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ce63b649279212f00e3",
+    "url": "/bible-reader-react/static/js/main.a41b6c66.chunk.js"
+  },
+  {
+    "revision": "1a5a05e65a2ff8111b21",
+    "url": "/bible-reader-react/static/js/runtime-main.b112b966.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/bible-reader-react/precache-manifest.5538e9655b14bb8048a02ede8b665e16.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/bible-reader-react/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
